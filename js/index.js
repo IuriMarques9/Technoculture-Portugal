0d9ceda2fc622fb4a07a5eb2da7d6d155fec2b68
Iuri Marques <iurimarcianom@hotmail.com>
2024-09-11 20:17:39 +0100

    change header

---
+++ js/index.js
@@ -0,0 +1,9 @@
+const header  = document.querySelector('#header');
+
+window.addEventListener('scroll',  function(){
+    if(window.scrollY > 0){
+        header.classList.add('!bg-[#590202]');
+    }else{
+        header.classList.remove('!bg-[#590202]');
+    }
+})
