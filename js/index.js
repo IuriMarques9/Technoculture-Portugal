2a6b1a733d3ce7746833af7a881d9ad7112bb5bf
Iuri Marques <iurimarcianom@hotmail.com>
2024-09-19 16:20:50 +0100

    "Updated CSS, HTML, and JavaScript files: removed navigation button styles, updated mobile navigation, and modified logo and social media icon behavior on page load and resize."

--- js/index.js
+++ js/index.js
@@ -1,8 +1,10 @@
+//#region Initialized variables
 const logo = document.querySelector('#logo'); /*Logo*/
 const menuIconAndSocial = document.querySelector('#menuIconAndSocial'); /*Icon Menu*/
 const header = document.querySelector('#header'); /*Header*/
 
-const navBiggerDevices = document.querySelector('#navBiggerDevices'); /*Nav for devices biger than mobile */
+const navBiggerDevices = document.querySelector('#navBiggerDevices'); /*Nav for devices bigger than mobile */
+const navMobile = document.querySelector('#navMobile'); /*Nav for mobile */
 
 const toTopButton = document.querySelector('#toTop'); /*Button to top*/
 
@@ -10,47 +12,50 @@ const cards = document.getElementsByName('card'); /*Gallery cards*/
 
 const divLeft = document.querySelector('#left'); /*Footer separator left*/
 const divRight = document.querySelector('#right'); /*Footer separator right*/
+//#endregion
 
-
-/*On page load choose the right logo for header and show/hide hamburguer menu icone*/ 
+//#region On page load conditional 
 if(window.screen.width < 640){
-    logo.src = "assets/img/logo.jpg";
-    menuIconAndSocial.classList.remove('hidden');
-}else{
-    logo.src = "assets/img/logo_grande.png";
-    navBiggerDevices.classList.remove('hidden');
-}
+    logo.src = "assets/img/logo.jpg";  /*Logo for mobile*/
+    menuIconAndSocial.classList.remove('hidden');  /*Icon Menu and Social buttons for mobile*/
 
-//#region Show/Hide footer divisory (<div>) and hamburguer menu icone, change logo if page rezise
+}else{
+    logo.src = "assets/img/logo_grande.png"; /*Logo for devices bigger than mobile*/
+    navBiggerDevices.classList.remove('hidden'); /*Icon Menu and Social buttons for bigger than mobile*/
+}
+//#endregion
+
+//#region On page resize
 addEventListener("resize", () => {
     if(window.screen.width < 640){
-        divLeft.classList.add('hidden');
-        divRight.classList.add('hidden');
-        logo.src = "assets/img/logo.jpg";
+        divLeft.classList.add('hidden'); //Hide left footer separator
+        divRight.classList.add('hidden'); //Hide right footer separator
+        logo.src = "assets/img/logo.jpg"; //Change logo from top bar
 
-        menuIconAndSocial.classList.remove('hidden');
-        console.log('entrou')
-        navBiggerDevices.classList.add('hidden');
+        menuIconAndSocial.classList.remove('hidden'); //Show hamburguer menu icone and social buttons from top bar
+        navBiggerDevices.classList.add('hidden'); //Hide nav for bigger devices
     }
     else{
-        divLeft.classList.remove('hidden');
-        divRight.classList.remove('hidden');
-        logo.src = "assets/img/logo_grande.png";
+        divLeft.classList.remove('hidden'); //Show left footer separator
+        divRight.classList.remove('hidden'); //Show right footer separator
+        logo.src = "assets/img/logo_grande.png"; //Change logo from top bar
+        menuIconAndSocial.classList.add('hidden'); //Hide hamburguer menu icone and social buttons from top bar
+        navBiggerDevices.classList.remove('hidden'); //Show nav for bigger devices
+        navMobile.classList.add('hidden'); //Hide mobile nav when screen is bigger than mobile
 
-        menuIconAndSocial.classList.add('hidden');
-        navBiggerDevices.classList.remove('hidden');
     }
 })
 //#endregion
 
-//#region Header Background color change and show/hide 'to top' button when scroll down */
+//#region On page scroll
 window.addEventListener('scroll',  function(){
     if(window.scrollY > 0){
-        header.classList.add('!bg-[#590202]');
-        toTopButton.classList.remove('invisible');
+        header.classList.add('!bg-[#590202]'); //Change header background color when scrolling to top
+        toTopButton.classList.remove('invisible'); //Show button to page top when scrolling to top
+
     }else{
-        header.classList.remove('!bg-[#590202]');
-        toTopButton.classList.add('invisible');
+        header.classList.remove('!bg-[#590202]'); //Change header background color when scrolling down
+        toTopButton.classList.add('invisible'); //Show button to page top when scrolling down
     }
 })
 //#endregion
